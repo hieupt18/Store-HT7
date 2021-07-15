3429cb5d7f63e1e220a4417d8bb8cdd8a51a30cd
Trung Hieu <trung.hieu@nelisoftwares.com>
2021-07-16 01:13:49 +0700

    fix: remove be

--- back-end/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import ProductRoutes from "./product";
-import { Application } from "express";
-
-export default (app: Application) => {
-  ProductRoutes(app);
-};
--- back-end/src/routes/product.ts
+++
@@ -1,6 +0,0 @@
-import { Application } from "express";
-import { ProductController } from "../controllers/product";
-
-export default (app: Application) => {
-  app.get("/product", ProductController.list);
-};
